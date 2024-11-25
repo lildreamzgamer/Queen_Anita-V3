f3a06ca173169fda4cbd39012e0a5ea431c3bad4
lildreamzgamer <lildreamzgamer@gmail.com>
2024-11-25 15:50:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '254745898330'
+global.nomerowner = ["254745898330"]
 
 // Apikey 
 
